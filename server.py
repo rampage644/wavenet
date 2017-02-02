ae250e5ca20c6040473b9eacc88f0cc4a69a8e9b
Sergei Turukin <rampage644@gmail.com>
2017-02-02 14:13:57 +0300

    Add weights and biases monitoring
    Work in progress. Commit just to save progress. Not working right now.

--- server.py
+++ server.py
@@ -1,63 +1,99 @@
 '''Server for visualization.'''
+#%%
 from __future__ import (absolute_import, print_function, unicode_literals, division)
 
 import json
-from random import random
+import itertools
 import numpy as np
 
-from bokeh.layouts import column
+from bokeh.layouts import column, gridplot
 from bokeh.models import Button
 from bokeh.palettes import Set1
-from bokeh.plotting import figure, curdoc
+from bokeh.plotting import figure, curdoc, output_notebook, show
+
+#%%
+def _keys_for(link):
+    data = ['W/data', 'W/grad', 'b/data', 'b/grad']
+    weights_biases = ['{}/{}/{}'.format(link, entry, key) for entry in data for key in SUFFIX_KEYS]
+    return ['{}/W-b/data/zeros'.format(link)] + weights_biases
+
+
+def plot_stats(p, ds, key):
+    def fill_between(x, y_lower, y_upper, alpha):
+        return p.patch(np.append(x, x[::-1]), np.append(y_lower, y_upper), alpha=alpha)
+
+    def get(suffix):
+        return ds.get(key + '/' + suffix, np.empty((1, 2)))
+
+    xt = get('mean')[:, 0]
+    y0 = fill_between(xt, get('min')[:, 1], get('max')[:, 1], 0.1)
+    y1 = fill_between(xt, get('percentile/0')[:, 1], get('percentile/6')[:, 1], 0.1)
+    y2 = fill_between(xt, get('percentile/1')[:, 1], get('percentile/5')[:, 1], 0.1)
+    y3 = fill_between(xt, get('percentile/2')[:, 1], get('percentile/4')[:, 1], 0.2)
+    y4 = p.line(xt, get('percentile/3')[:, 1])
+
+    return [y0, y1, y2, y3, y4]
+
 
 FILENAME = 'log'
-
-# create a plot and style its properties
-p = figure(plot_width=1200, plot_height=800)
 COLORS = Set1[3][:2] * 2
 ALPHAS = [.4, .4, 1., 1.]
-loss = p.multi_line(xs=[[]] * 4,
-                    ys=[[]] * 4,
-                    color=COLORS,
-                    alpha=ALPHAS)
-
-# # create a callback that will add a number in a random location
 TRAIN_KEY, TEST_KEY = 'main/nll', 'validation/main/nll'
+TIME_KEY = 'iteration'
 WINDOW_SIZE = 10
-
-
 PREFIX_KEYS = [
     'predictor'
 ]
-SUFFIX_KEYS = [
-    '{}/max',
-    '{}/mean',
-    '{}/min',
-    '{}/percentile/0',
-    '{}/percentile/1',
-    '{}/percentile/2',
-    '{}/percentile/3',
-    '{}/percentile/4',
-    '{}/percentile/5',
-    '{}/percentile/6',
-    '{}/std',
-]
-def _keys(symbol):
-    return [key.forma(symbol) for key in SUFFIX_KEYS]
+SUFFIX_KEYS = ['max', 'mean', 'min', 'percentile/0', 'percentile/1', 'percentile/2',
+               'percentile/3', 'percentile/4', 'percentile/5', 'percentile/6', 'std']
+DATA_KEYS = [TRAIN_KEY, TEST_KEY] + list(itertools.chain(*[_keys_for(link) for link in PREFIX_KEYS]))
 
-KEYS = _keys('W/data') + _keys('W/grad') + _keys('b/data') + _keys('b/grad')
+#%%
+loss_plot = figure(plot_width=1000, plot_height=800)
+loss = loss_plot.multi_line(xs=[[]] * 4,
+                            ys=[[]] * 4,
+                            color=COLORS,
+                            alpha=ALPHAS)
+
+plots = []
+dataseries = {}
+source = {}
+
+
+for prefix in PREFIX_KEYS:
+    for key in ['W/data', 'W/grad', 'b/data', 'b/grad']:
+        complex_key = prefix+'/'+key
+        p = figure(title=complex_key)
+        source[complex_key] = plot_stats(p, dataseries, complex_key)
+        plots.append(p)
+    for key in ['W-b/data/zeros']:
+        def get(key):
+            return dataseries.get(key, np.empty((1, 2)))
+        complex_key = prefix+'/'+key
+        p = figure(title=complex_key)
+        source[complex_key] = p.line(get(complex_key)[:, 0], get(complex_key)[:, 1])
+        plots.append(p)
+
+
+grid = gridplot(plots, plot_width=250, plot_height=250, ncols=5)
 
 
 def callback():
     with open(FILENAME) as ifile:
         data = json.load(ifile)
-        train_ts = np.array([(record['iteration'], record[TRAIN_KEY]) for record in data], 'f')
-        test_ts = np.array([(record['iteration'], record[TEST_KEY]) for record in data if TEST_KEY in record], 'f')
+        for key in DATA_KEYS:
+            dataseries[key] = np.array([
+                (rcrd[TIME_KEY], rcrd[key]) for rcrd in data if key in rcrd], 'f')
+
 
     window = np.hamming(WINDOW_SIZE)
     window /= window.sum()
+
     def smooth(data):
         return np.concatenate([data[:WINDOW_SIZE-1], np.convolve(data, window, mode='valid')])
+
+    train_ts = dataseries['main/nll']
+    test_ts = dataseries['validation/main/nll']
 
     train_smooth = smooth(train_ts[:, 1])
     test_smooth = smooth(test_ts[:, 1])
@@ -72,5 +108,9 @@ def callback():
 button = Button(label="Update")
 button.on_click(callback)
 
-# put the button and plot in a layout and add to the document
-curdoc().add_root(column(button, p))
+
+# output_notebook()
+layout = column(button, loss_plot, grid)
+curdoc().add_root(layout)
+
+# show(layout)
