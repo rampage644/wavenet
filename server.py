cdada993cc11e4a9eb8d24adfe013095cbbce12f
Sergei Turukin <rampage644@gmail.com>
2017-02-03 11:17:25 +0300

    Visual tool completed
    Very simple visualization tool with `bokeh` server. Reads log file
    generated by `chainer` and plots losses and layers weights, biases
    sparsity and update-to-parameter ratio. Statistics is collected
    with `ParameterStatistics` extension found at some `chainer` PR.
    Should be heavily refactored for future use.

--- server.py
+++ server.py
@@ -9,31 +9,42 @@ import numpy as np
 from bokeh.layouts import column, gridplot
 from bokeh.models import Button
 from bokeh.palettes import Set1
-from bokeh.plotting import figure, curdoc, output_notebook, show
+from bokeh.plotting import figure, curdoc, output_notebook, show, output_server
 
 #%%
 def _keys_for(link):
     data = ['W/data', 'W/grad', 'b/data', 'b/grad']
     weights_biases = ['{}/{}/{}'.format(link, entry, key) for entry in data for key in SUFFIX_KEYS]
-    return ['{}/W-b/data/zeros'.format(link)] + weights_biases
+    stats = ['{}/{}'.format(link, key) for key in ['W/data-grad/ratio', 'b/data-grad/ratio', 'W-b/data/zeros']]
+    return stats + weights_biases
 
 
-def plot_stats(p, ds, key):
-    def fill_between(x, y_lower, y_upper, alpha):
-        return p.patch(np.append(x, x[::-1]), np.append(y_lower, y_upper), alpha=alpha)
+def convert_to_patch(xt, y_lower, y_upper):
+    return {
+        'x': np.append(xt, xt[::-1]),
+        'y': np.append(y_lower, y_upper)
+    }
 
-    def get(suffix):
-        return ds.get(key + '/' + suffix, np.empty((1, 2)))
 
-    xt = get('mean')[:, 0]
-    y0 = fill_between(xt, get('min')[:, 1], get('max')[:, 1], 0.1)
-    y1 = fill_between(xt, get('percentile/0')[:, 1], get('percentile/6')[:, 1], 0.1)
-    y2 = fill_between(xt, get('percentile/1')[:, 1], get('percentile/5')[:, 1], 0.1)
-    y3 = fill_between(xt, get('percentile/2')[:, 1], get('percentile/4')[:, 1], 0.2)
-    y4 = p.line(xt, get('percentile/3')[:, 1])
+def plot_stats(p, key):
+    _get = get_for(key)
+
+    y0 = p.patch(x=[], y=[], alpha=0.1)
+    y1 = p.patch(x=[], y=[], alpha=0.1)
+    y2 = p.patch(x=[], y=[], alpha=0.1)
+    y3 = p.patch(x=[], y=[], alpha=0.2)
+    y4 = p.line(x=[], y=[])
 
     return [y0, y1, y2, y3, y4]
 
+
+def get(key):
+    return dataseries.get(key, np.empty((1, 2)))
+
+def get_for(key):
+    def _get(suffix):
+        return get(key + '/' + suffix)
+    return _get
 
 FILENAME = 'log'
 COLORS = Set1[3][:2] * 2
@@ -42,7 +53,7 @@ TRAIN_KEY, TEST_KEY = 'main/nll', 'validation/main/nll'
 TIME_KEY = 'iteration'
 WINDOW_SIZE = 10
 PREFIX_KEYS = [
-    'predictor'
+    'conv1', 'conv2', 'conv4'
 ]
 SUFFIX_KEYS = ['max', 'mean', 'min', 'percentile/0', 'percentile/1', 'percentile/2',
                'percentile/3', 'percentile/4', 'percentile/5', 'percentile/6', 'std']
@@ -64,18 +75,16 @@ for prefix in PREFIX_KEYS:
     for key in ['W/data', 'W/grad', 'b/data', 'b/grad']:
         complex_key = prefix+'/'+key
         p = figure(title=complex_key)
-        source[complex_key] = plot_stats(p, dataseries, complex_key)
+        source[complex_key] = plot_stats(p, complex_key)
         plots.append(p)
-    for key in ['W-b/data/zeros']:
-        def get(key):
-            return dataseries.get(key, np.empty((1, 2)))
+    for key in ['W/data-grad/ratio', 'b/data-grad/ratio', 'W-b/data/zeros']:
         complex_key = prefix+'/'+key
         p = figure(title=complex_key)
         source[complex_key] = p.line(get(complex_key)[:, 0], get(complex_key)[:, 1])
         plots.append(p)
 
 
-grid = gridplot(plots, plot_width=250, plot_height=250, ncols=5)
+grid = gridplot(plots, plot_width=250, plot_height=250, ncols=7)
 
 
 def callback():
@@ -84,6 +93,8 @@ def callback():
         for key in DATA_KEYS:
             dataseries[key] = np.array([
                 (rcrd[TIME_KEY], rcrd[key]) for rcrd in data if key in rcrd], 'f')
+            if not len(dataseries[key]):
+                del dataseries[key]
 
 
     window = np.hamming(WINDOW_SIZE)
@@ -92,8 +103,8 @@ def callback():
     def smooth(data):
         return np.concatenate([data[:WINDOW_SIZE-1], np.convolve(data, window, mode='valid')])
 
-    train_ts = dataseries['main/nll']
-    test_ts = dataseries['validation/main/nll']
+    train_ts = get('main/nll')
+    test_ts = get('validation/main/nll')
 
     train_smooth = smooth(train_ts[:, 1])
     test_smooth = smooth(test_ts[:, 1])
@@ -104,13 +115,37 @@ def callback():
     })
 
 
+    for k, v in source.items():
+        if isinstance(v, list):
+            _get = get_for(k)
+
+            xt = _get('mean')[:, 0]
+            v[0].data_source.data.update(
+                convert_to_patch(xt, _get('min')[:, 1], _get('max')[:, 1]))
+            v[1].data_source.data.update(
+                convert_to_patch(xt, _get('percentile/0')[:, 1], _get('percentile/6')[:, 1]))
+            v[2].data_source.data.update(
+                convert_to_patch(xt, _get('percentile/1')[:, 1], _get('percentile/5')[:, 1]))
+            v[3].data_source.data.update(
+                convert_to_patch(xt, _get('percentile/2')[:, 1], _get('percentile/4')[:, 1]))
+
+            v[-1].data_source.data.update({
+                'x': _get('percentile/3')[:, 0],
+                'y': _get('percentile/3')[:, 1],
+            })
+        else:
+            v.data_source.data.update({
+                'x': get(k)[:, 0],
+                'y': get(k)[:, 1],
+            })
+
+
 # # add a button widget and configure with the call back
 button = Button(label="Update")
 button.on_click(callback)
-
 
 # output_notebook()
 layout = column(button, loss_plot, grid)
 curdoc().add_root(layout)
 
-# show(layout)
+
